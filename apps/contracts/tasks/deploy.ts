5a43321985f5cd71ce800894ac79bd60d28439c2
Tihe <tihechen@gmail.com>
2024-03-16 17:35:07 +0800

    update poseidonT3 lib on script

--- apps/contracts/tasks/deploy.ts
+++ apps/contracts/tasks/deploy.ts
@@ -4,7 +4,15 @@ task("deploy", "Deploy a Feedback contract")
     .addOptionalParam("logs", "Print the logs", true, types.boolean)
     .setAction(async ({ logs }, { ethers, run }) => {
 
-        const AnonymousWhistleblowerFactory = await ethers.getContractFactory("AnonymousWhistleblower")
+        const PoseidonT3Factory = await ethers.getContractFactory("PoseidonT3");
+        const poseidonT3Contract = await PoseidonT3Factory.deploy();
+        const poseidonT3Address = await poseidonT3Contract.getAddress();
+
+        const AnonymousWhistleblowerFactory = await ethers.getContractFactory("AnonymousWhistleblower", {
+            libraries: {
+                PoseidonT3: poseidonT3Address,
+            }
+        })
         // depth 16
         const anonymousWhistleblowerContract = await AnonymousWhistleblowerFactory.deploy(16)
 
